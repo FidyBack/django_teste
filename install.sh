a865787367f56c8fe598ae3d949b48aca678c0c8
FidyBack <abel.cavalcante.ismart@gmail.com>
2021-11-24 22:24:29 -0300

    Mudanças

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-python manage.py migrate
-python3 manage.py runserver 0:8080
+python3 manage.py migrate
 
 export DJANGO_SUPERUSER_PASSWORD=cloud
 export DJANGO_SUPERUSER_USERNAME=cloud
 export DJANGO_SUPERUSER_EMAIL=cloud@a.com
-python manage.py createsuperuser  --noinput
+python3 manage.py createsuperuser  --noinput
+
+python3 manage.py runserver 0:8080
